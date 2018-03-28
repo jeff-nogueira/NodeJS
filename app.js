f11579a2a866d091f425862d7373eb3e6fbfb7b7
jeff nogueira <36955545+jeff-nogueira@users.noreply.github.com>
2018-03-27 22:59:52 -0300

    mongoq
    Simples controller explorando funcionalidades do MongoQ

--- app.js
+++ app.js
@@ -1,21 +1,59 @@
+// Instanciando os módulos Express e Mongoq
 var express = require('express');
-var app = express();
-
-app.get('/', function(req, res){
-	console.log("Welcome API");
+var mongoq = require('mongoq');
+// Configurando a execução do banco MongoDB
+var COLLECTION = 'collection_teste';
+var DB = 'banco_teste';
+var db = mongoq(DB);
+var collection = db.collection(COLLECTION);
+// Iniciando o servidor Express
+var app = module.exports = express.createServer();
+// Configuração básica do servidor Express
+app.configure(function(){
+   app.use(express.bodyParser());
+   app.use(express.methodOverride());
+   app.use(app.router);
 });
-
-var users =[ 
-   {id:1, name:'Joshua', email:'joshua@example.com'}
-   ,
-   {id:2, name:'Peter', email:'peter@example.com'}
-   ,
-   {id:3, name:'john', email:'john@example'}
-
-];
-
-app.get('/api/get_users', function(req, res){
-	res.send(users);
+// Rota GET para listar os usuários
+app.get('/usuarios', function(req, res){
+   // Fazendo uma consulta no banco de dados
+   collection.find().toArray(function(err, result){
+      res.render('index', { usuarios : result });
+   });
 });
-
-app.listen(8080);
+// Rota GET para consultar um usuário
+app.get('/usuario/:id', function(req, res){
+   // Recebendo os parâmetros de um query string
+   var id = req.params.id;
+   // Fazendo uma consulta no banco de dados
+   var params = {usuario : {id: id}};
+   collection.find(params).toArray(function(err, result){
+      res.render('ver', { usuario : result.usuario });
+   });
+});
+// Rota POST para cadastrar um usuário
+app.post('/usuario', function(req, res){
+   // Recebendo os parâmetros da requisição
+   var usuario = req.body.usuario;
+   // Persistindo o novo usuário
+   collection.insert({usuario : usuario});
+   res.redirect('/usuarios');
+});
+// Rota PUT para atualizar um usuário
+app.put('/usuario/:id', function(req, res){
+   // Recebendo os parâmetros de um query string
+   var id = req.params.id;
+   // Recebendo os parâmetros da requisição
+   var usuario = req.body.usuario;
+   // Atualizando dados do usuário que possuir este id
+   collection.update({usuario: {id: id }}, {usuario : usuario});
+       res.redirect('/usuarios');
+   });
+// Rota DELETE para excluir um usuário
+app.del('/usuario/:id', function(req, res){
+   // Recebendo os parâmetros de um query string
+   var id = req.params.id;
+   // Excluindo o usuário do banco de dados
+   collection.remove({usuario : {id: id }});
+   res.redirect('/usuarios');
+});
